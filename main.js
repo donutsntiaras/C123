46f889b4f1ceaced437190e43244801035f753fd
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-12-08 15:33:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup() {
     video=createCapture(VIDEO);
     video.size(550,550);
@@ -7,6 +13,11 @@ function setup() {
 
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+
+    /*
+    row="<marquee> Developed by WhiteHatJr Team</marquee>";
+    document.getElementById("n1").innerHTML=row;
+    */
 }
 
 function modelLoaded() {
@@ -15,10 +26,24 @@ function modelLoaded() {
 
 function draw() {
     background('#cee8e4');
+
+    document.getElementById("square_side").innerHTML=" Width And Height of a Square will be=" + difference + "px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY =" + noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+
+        console.log("leftWristX =" + leftWristX + "rightWristX =" + rightWristX + "difference =" + difference);
     }
 }
